d81b19c14c6d7fc6eb69ac04e9150df9f14a3c07
RobinJehn <robin.jehn@gmx.de>
2025-03-30 17:29:42 +0100

    fix max retransmission

--- sliding_window/Sender3.py
+++ sliding_window/Sender3.py
@@ -34,7 +34,7 @@ class GoBackN:
         self.done = False
         self.consecutive_retransmissions = 0
         # When dropping the last ack we need to terminate
-        self.max_retransmissions = 50
+        self.max_retransmissions = max(50, self.window_size * 5)
 
     def start_timer(self):
         if self.timer:
